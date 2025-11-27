08d99dbb0638764931c063d30a8c8e1a99d3ace5
Akbar Baghbani <abaghbani@gmail.com>
2025-11-27 10:19:49 +0100

    docker build script is modified to set target and merge output files as a single binary file

--- docker_build.sh
+++ docker_build.sh
@@ -8,12 +8,18 @@ docker run --rm \
   -w /project \
   -u $UID \
   -e HOME=/tmp \
+  -it \
   espressif/idf:latest \
-  idf.py build
-
-
+  # idf.py set-target esp32h2
+  # idf.py build
+  # idf.py merge-bin -o my-merged-binary.bin -f raw
+  
+  
 # -u $UID: makes the command run with your user ID so that files are created as you (instead of root).
 # -e HOME=/tmp: gives the user a home directory for storing temporary files created by idf.py in ~/.cache.
 
 ##### Note
 # Commands which communicate with the development board, such as idf.py flash and idf.py monitor does not work in the container, unless the serial port is passed through into the container.
+# important-1: before build a project set-target should be implied
+# important-2: output is usually 3 files (bootloader(0x0), partition-table(0x8000), application(0x10000))
+# merge-bin can merge all 3 binary files and make a single files flashing from address 0x0
